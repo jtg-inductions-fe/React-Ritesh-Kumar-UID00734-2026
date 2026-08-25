ca4765721770e4c45db1e2df413387140956b0f0
riteshkumartshk <ritesh.kumar1@joshtechnologygroup.com>
2026-08-25 18:42:31 +0530

    [RK_A4_04]: remove header component and rename constant files

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export * from './SearchAutocomplete/SearchAutocomplete.component';
+export * from './SearchAutocomplete/SearchAutocomplete.types';
+
+export * from './UserInfo/UserInfo.component';
+export * from './UserInfo/UserInfo.types';
+
+export * from './UserMenu/UserMenu.component';
+export * from './UserMenu/UserMenu.types';
